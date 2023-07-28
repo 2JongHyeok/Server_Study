cdd7e13d9520072a05a7fcf6ea1ba81c3ecb4c13
2JongHyeok <104877754+2JongHyeok@users.noreply.github.com>
2023-07-28 22:26:00 +0900

    템플릿을 쓴 Lock
    MMO 게임같이 코드가 길어지면
    Lock과 Unlock을 할때 까먹을 수 있기 때문에
    RAII를 사용하여 자동으로 Lock이 호출되고 함수가 끝나면 Unlock이 될수 있게 LockGuard 만들어봄

--- Server_Code/GameServer/GameServer.cpp
+++ Server_Code/GameServer/GameServer.cpp
@@ -11,17 +11,37 @@ vector<int32> v;
 // Mutal Exclusive ( 상호베타적 )
 mutex m;
 
+// RAII ( Resource Acquisition is Initialization )
+template<typename T>
+class LockGuard
+{
+public:
+	LockGuard(T& m)
+	{
+		_mutex = &m;
+		_mutex->lock();
+	}
+
+	~LockGuard()
+	{
+		_mutex->unlock();
+	}
+private:
+	T* _mutex;
+};
+
 void Push()
 {
 	for (int32 i = 0; i < 10000; ++i)
 	{
 		// 자물쇠 잠그기
-		m.lock();
+		LockGuard<std::mutex> lockGuard(m);
+		//m.lock();
 
 		v.push_back(i);
 		
 		// 자물쇠 풀기
-		m.unlock();
+		//m.unlock();
 	}
 }
 
