b821fff95fb20515cead125b0c1e25b8570296ec
2JongHyeok <104877754+2JongHyeok@users.noreply.github.com>
2023-07-28 19:38:46 +0900

    Thread Join에 대해 배움

---
+++ Server_Code/GameServer/GameServer.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+
+#include <thread>
+
+void HelloThread()
+{
+    cout << "Hello Thread" << endl;
+}
+
+int main()
+{
+    std::thread t(HelloThread);
+
+    cout << "Hello Main" << endl;
+    
+    t.join();   // main 쓰레드가 t쓰레드가 끝날때 까지 기다려주는 코드
+}
