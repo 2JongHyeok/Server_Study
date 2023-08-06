05790467b4092c14d975c9ebfc44e854c3723deb
2JongHyeok <104877754+2JongHyeok@users.noreply.github.com>
2023-08-07 03:00:56 +0900

    Condition Variable

--- Server_Code/GameServer/GameServer.cpp
+++ Server_Code/GameServer/GameServer.cpp
@@ -8,19 +8,27 @@
 
 mutex m;
 queue<int32> q;
-HANDLE handle;
+
+// 참고) CV는 User-Level Object ( 커널 오브젝트 x )
+condition_variable cv;
 
 void Producer()
 {
 	while (true)
 	{
+		// 1) Lock을 잡고
+		// 2) 공유 변수 값을 수정
+		// 3) Lock을 풀고
+		// 4) 조건변수 통해 다른 쓰레드에게 통지
+
 		{
 			unique_lock<mutex> lock(m);
 			q.push(100);
 		}
 
-		::SetEvent(handle);
-		this_thread::sleep_for(10000ms);
+		cv.notify_one();	// wait중인 쓰레드가 있으면 딱 1개를 깨운다.
+
+		//this_thread::sleep_for(10000ms);
 	}
 }
 
@@ -28,33 +36,31 @@ void Consumer()
 {
 	while (true)
 	{
-		::WaitForSingleObject(handle, INFINITE);
-		//::ResetEvent(handle); (만약 CreateEvent의 두번째 인자를 True로 바꾸면 이 코드로 직접 바꿔줘야함)
-		// Non-Signal
-
 		unique_lock<mutex> lock(m);
-		if (q.empty() == false)
+		cv.wait(lock, []() {return q.empty() == false; });
+		// 1) Lock을 잡고
+		// 2) 조건 확인
+		// - 만족 O => 빠져나와서 이어서 코드를 진행
+		// - 만족 X => Lock을 풀어주고 대기 상태
+		
+		// 그런데 notify_one을 했으면 항상 조건식을 만족하는거 아닐까?
+		// Spurious Wakeup (가짜 기상?)
+		// notify_one할때 Lock을 잡고 있는 것이 아니기 때문.
+
+		//while (q.empty() == false)
 		{
 			int32 data = q.front();
 			q.pop();
-			cout << data << endl;
+			cout << q.size() << endl;
 		}
 	}
 }
 
 int main()
 {
-	// 커널 오브젝트
-	// Usage Count
-	// Signal ( 파란불 ) / Non-Signal (빨간불) << bool
-	// Auto / Manual << bool
-	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*bManualReset*/, FALSE/*bInitialState*/, NULL);
-	
 	thread t1(Producer);
 	thread t2(Consumer);
 
 	t1.join();
 	t2.join();
-
-	::CloseHandle(handle);
 }
