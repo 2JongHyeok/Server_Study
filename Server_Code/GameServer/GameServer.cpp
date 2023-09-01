673af5a39e03973f5c6644ee1686622d6a2a4d07
2JongHyeok <104877754+2JongHyeok@users.noreply.github.com>
2023-09-01 20:37:13 +0900

    더이상 쓰지 않는 코드 삭제 및 코드 초기화

--- Server_Code/GameServer/GameServer.cpp
+++ Server_Code/GameServer/GameServer.cpp
@@ -6,43 +6,8 @@
 #include <mutex>
 #include <Windows.h>
 #include <future>
-#include "ConcurrentQueue.h"
-#include "ConcurrentStack.h"
-
-LockFreeQueue<int32> q;
-LockFreeStack<int32> s;
-
-void Push()
-{
-	while (true)
-	{
-		int32 value = rand() % 100;
-		q.Push(value);
-
-		this_thread::sleep_for(1ms);
-	}
-}
-
-void Pop()
-{
-	while (true)
-	{
-		auto data = q.TryPop();
-		if (data != nullptr)
-			cout << (*data) << endl;
-	}
-}
 
 int main()
 {
-	shared_ptr<int32> ptr;
-	bool value = atomic_is_lock_free(&ptr);
 
-	thread t1(Push);
-	thread t2(Pop);
-	thread t3(Pop);
-
-	t1.join();
-	t2.join();
-	t3.join();
 }
