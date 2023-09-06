a2bf7b9fc343541e02ab5ce25ce59e02dbe03421
2JongHyeok <104877754+2JongHyeok@users.noreply.github.com>
2023-09-06 17:58:53 +0900

    멀티쓰레드를 활용한 소수 구하기 연습문제

--- Server_Code/GameServer/GameServer.cpp
+++ Server_Code/GameServer/GameServer.cpp
@@ -1,38 +1,73 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
-#include <thread>
 #include <atomic>
 #include <mutex>
 #include <Windows.h>
 #include <future>
 #include "ThreadManager.h"
 
-#include "PlayerManager.h"
-#include "AccountManager.h"
+#include <vector>
+#include <thread>
+
+// 소수 구하기
+
+// 1과 자기 자신으로만 나뉘면 그것을 소수라고 함
+
+bool IsPrime(int number)
+{
+	if (number <= 1)
+		return false;
+	if (number == 2 || number == 3)
+		return true;
+
+	for (int i = 2; i < number; ++i)
+	{
+		if ((number % i) == 0)
+			return false;
+	}
+
+	return true;
+}
+
+int CountPrime(int start, int end)
+{
+	int count = 0;
+
+	for (int number = start; number <= end; ++number)
+	{
+		if (IsPrime(number))
+			count++;
+	}
+
+	return count;
+}
 
 int main()
 {
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "PlayerThenAccount" << endl;
-				GPlayerManager.PlayerThenAccount();
-				this_thread::sleep_for(100ms);
-			}
-		});
+	const int MAX_NUMBER = 10000;
 
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "AccountThenPlayer" << endl;
-				GAccountManager.AccountThenPlayer();
-				this_thread::sleep_for(100ms);
-			}
-		});
+	// 1-MAX_NUMBER까지의 소수 개수
 
+	vector<thread> threads;
 
-	GThreadManager->Join();
+	int coreCount = thread::hardware_concurrency();
+	int jobCount = (MAX_NUMBER / coreCount) + 1;
+
+	atomic<int> primeCount = 0;
+
+	for (int i = 0; i < coreCount; ++i)
+	{
+		int start = (i * jobCount) + 1;
+		int end = min(MAX_NUMBER, ((i + 1) * jobCount));
+
+		threads.push_back(thread([start, end, &primeCount]() {
+			primeCount += CountPrime(start, end);
+		}));
+	}
+
+	for (thread& t : threads)
+		t.join();
+
+	cout << primeCount << endl; 
 }
